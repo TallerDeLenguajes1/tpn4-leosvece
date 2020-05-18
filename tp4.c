30d2f32ef21c85bfc8a5537fc4012f208b53d423
Leo <programa0.1@outlook.com>
2020-05-18 18:25:45 -0300

    ultima correccion

--- tp4.c
+++ tp4.c
@@ -3,124 +3,228 @@
 #include<string.h>
 #include<time.h>
 
-
-//problema con el codigo
-/*despues de cargar los datos, elijo dos tareas a realizar para iniciar una prueba. Cuando interactuo para determinar cuales de las tareas se realizaron,
-si eligo que la primera fue realizada, me muestra el programa que la tarea fue realizada, pero solo me muestra el ID de la pendiente.
-si eligo que la segunda fue realizada, me muestra como que todas fueron realizadas y, en este caso, me muestra las tareas pendientes,
-que en el anterio eleccion no me permitia*/
-
-typedef struct Tarea {
+typedef struct Tarea{
 int TareaID; //Numerado en ciclo iterativo
-char *Descripcion; //
-int Duracion; // entre 10 â€“ 100
+char * Descripcion; //
+int Duracion; // entre 10 ï¿½ 100
 }tarea;
 
+typedef struct nodo{
+tarea T;
+struct nodo *Siguiente;
+}nodo;
+
+void cargarTarea(nodo **star,int x);
+void mostrar(nodo **star);
+//nodo *finalizarTarea(nodo **star);
+nodo *estadoDetarea(nodo **star);
+void busquedas(nodo **lista);
+nodo *busquedaID(int id, nodo **lista);
+nodo *busquedaNombre(char nombre[], nodo **lista);
 
 
-void cargarTarea(tarea **T,int x);
-void mostrar(tarea **T, int x);
-void estadoDetarea(tarea **R, tarea **P, int x);
-//void listarTarea(tarea **T,int x);
+
 
 int main(){
-    tarea *tareasPendientes, *tareasRealizadas;
+	nodo *lista =NULL, listaSup;
+    nodo *tareaPend= NULL;
+    nodo *tareaReal=NULL, *aux;
     srand(time(NULL));
-    int x;
-    
-    printf("Cuantas tareas se deben realizar: ");
-    scanf("%d",&x);
-    fflush(stdin);
-    
-    tareasPendientes = (tarea*)malloc((x+1)*sizeof(tarea));
-    if (tareasPendientes== NULL){
-    	printf("compre otra pc perro no tiene memoria");
+    int x,i, band;
+	
+	printf("ingrese la cantidad de tareas a realizar: ");
+	scanf("%d",&x);
+	fflush(stdin);
+	
+	cargarTarea(&lista,x);
+	
+	do {
+		busquedas(&lista);
+		printf("la realizar otra tarea?\n1)si		2)no\n",i);
+		scanf("%d",&band);
+	}while(band==1);
+	
+
+	for(i=0;i<x;i++){
+		
+		printf("la tarea numero %d fue realizada?\n1)si		2)no\n",i)+1;
+		scanf("%d",&band);
+		aux = estadoDetarea(&lista);
+		
+		if(band ==1){
+			
+			tareaReal = (nodo*)malloc(sizeof(nodo));
+			tareaReal->Siguiente =  aux;
+			aux = tareaReal;
+			tareaReal = tareaReal->Siguiente; //sin esto inicializa el primero como basura ¿porque?
+		}
+		else{
+			
+			tareaPend = (nodo*)malloc(sizeof(nodo));
+			tareaPend->Siguiente =  aux;
+			aux = tareaReal;
+			tareaPend = tareaPend->Siguiente;	//sin esto inicializa el primero como basura ¿porque?
+		}	
 	}
 	
-	tareasRealizadas = (tarea*)malloc((x+1)*sizeof(tarea));
-    if (tareasRealizadas== NULL){
-    	printf("compre otra pc perro no tiene memoria");
+	
+	if(tareaReal != NULL){
+	
+	printf("\nlas tareas realizadas son:");
+	mostrar(&tareaReal);
 	}
 	
+	if(tareaPend !=NULL){
+
+	printf("\nTareas pendientes\n");
+	mostrar(&tareaPend);
+	}	
+
 	
-	cargarTarea(&tareasPendientes,x);
-	estadoDetarea(&tareasRealizadas,&tareasPendientes,x);
-	//listarTarea(&tareasPendientes,x);
+	free(lista);
+	free(tareaReal);
+	free(tareaPend);
 	
-	printf("las tareas realizadas son:");
-	mostrar(&tareasRealizadas,x);
-	
-	printf("\n tareas pendientes\n");
-	mostrar(&tareasPendientes,x);
-	
-	free(tareasPendientes);
-	free(tareasRealizadas);
     return 0;
 }
 
-void cargarTarea(tarea **T, int x){
-   char aux[100];
-    int i;
-	tarea *t = *T;
 
-    for (i=0;i<x;i++){
-    	t[i].TareaID = i;
-    	printf("%d) de una descripcion: ",i+1);
-    	gets(aux);
-    	t[i].Descripcion = (char*)malloc((strlen(aux)+1)*sizeof(char));
-    	strcpy(t[i].Descripcion,aux);
-    	t[i].Duracion =10+ rand()%100;
-	}  
-    
+void busquedas(nodo **lista){
+	nodo * aux = *lista;
+	nodo *busqueda =NULL;
+	int buscar, id;
+	char nombre[100];
+	
+	printf("¿que tarea desea buscar? \n1)por ID    2)por nombre");
+	scanf("%d",&buscar);
+		
+		if(buscar ==1){
+			printf("escriba el ID de la tarea que busca");
+			scanf("%d",&id);
+			busqueda = busquedaID(id,&aux);
+			mostrar(&busqueda);
+	
+		}else{
+			printf("escriba el nombre de la tarea:\n");
+			fflush(stdin);
+			gets(nombre);
+			busqueda = busquedaNombre(nombre,&aux);
+			mostrar(&busqueda); 
+		}
+	
 }
 
-void estadoDetarea(tarea **R, tarea **P, int x){
+void cargarTarea(nodo **star, int x){
+   	char aux1[100];
+    int i=0;
+	nodo *aux;	
 	
-	tarea *p=*P, *r= *R;
-	//tarea *aux = NULL;
-	int i,j=0, band=0, aux = NULL;
-	
-	for (i=0;i<x;i++){
-		printf("la tarea numero %d fue realizada?\n1)si		2)no\n",i);
-		scanf("%d",&band);
-		fflush(stdin);
-		if(band ==1){
-			r[j] = p[i]; //p =pendiente r=realizada
-			p[i].Descripcion = NULL;
-			p[i].Duracion = aux;
-			p[i].TareaID= aux;
-			j++;
-		}
-		else{
-			r[j].Descripcion = NULL;
-			r[j].Duracion = aux;
-			r[j].TareaID = aux;
-			j++;
-		}
-	}	
-}
-/*
-void listarTarea(tarea **T,int x){
-	int i;
-	tarea *t=*T;
+	aux = (nodo*)malloc(sizeof(nodo));
 	
 	for(i=0;i<x;i++){
-		if( t[i].Descripcion == NULL){
-			t[i].Descripcion = t[i+1].Descripcion;
-			t[i].Duracion = t[i+1].Duracion;
-			t[i].TareaID=t[i+1].TareaID;
-		}	
+		*star = (nodo*)malloc(sizeof(nodo));
+
+	 //con esto el forma carga una basura ¿porque?
+	/* 	(*star)->T.TareaID = i;
+	    printf("%d) de una descripcion: ",i+1);
+	    gets(aux1);
+	    (*star)->T.Descripcion = (char*)malloc((strlen(aux1)+1)*sizeof(char));
+	    strcpy((*star)->T.Descripcion,aux1);
+	    (*star)->T.Duracion =10+ rand()% 90; 
+	    (*star)->Siguiente = NULL;
+	    
+	    
+	   	(*star)->Siguiente = aux;
+		aux = *star;
+		*star = (*star)->Siguiente;*/ 
+
+		
+		aux->T.TareaID = i;
+	    printf("%d) de una descripcion: ",i+1);
+	    gets(aux1);
+	    aux->T.Descripcion = (char*)malloc((strlen(aux1)+1)*sizeof(char));
+	    strcpy(aux->T.Descripcion,aux1);
+	    aux->T.Duracion =10+ rand()% 90;
+	    //aux->Siguiente = NULL; // si cargo esto se llena de basura
+	    
+	    (*star)->Siguiente = aux;
+		aux = *star;
+		*star = (*star)->Siguiente; // si no pongo esto, el primero se llena de basura ¿porque?
+	    
 	}
+
+}
+
+
+nodo *estadoDetarea(nodo **star){
+	
+	nodo  *Pendiente = *star, *aux =NULL;
+
+		//aux = finalizarTarea(&Pendiente);
+		//####################################recordatorio############################
+		// al inicializar con pendiente se realizaba una copia, o eso es lo que creo, y no modificaba el original, haciendo que se copie todo
+			
+		aux = *star;
+		*star = (*star)->Siguiente;
+		aux->Siguiente = NULL; // con esto las dos guardan el mismo nodo, el ultimo, en vez de solo guardar el que pido, sin esto guardan los dos nodos todos los nodos
+		
+		return aux;	
+	
+	
+}
+/*
+nodo *finalizarTarea(nodo **star){
+	nodo *aux = *star; 
+	*star = (*star)->Siguiente;
+	return aux; 
 }*/
 
-void mostrar(tarea **T, int x){
-	tarea *t = *T;
-	int i, aux = -1;
-	 for (i=0;i<x;i++){
-	 	if(t[i].Descripcion != NULL){
-    		printf("\nID: %d",t[i].TareaID);
-    		printf("\nDescripcion: %s",t[i].Descripcion);
-    		printf("\nDuracion: %d\n",t[i].Duracion);
-    	}
-	}	
+
+nodo *busquedaID(int id, nodo **lista){
+	nodo *aux = *lista;
+	while ( aux->Siguiente && aux->T.TareaID != id){
+		aux = aux->Siguiente;
+	}
+	if (aux->Siguiente != NULL){
+		aux->Siguiente = NULL;
+		return aux;		
+	}
+	else{
+		printf("su tarea no existe");
+		return NULL;
+	}
+
+}
+
+nodo *busquedaNombre(char nombre[], nodo **lista){
+	nodo *aux = *lista;
+	
+	while ( aux->Siguiente && strcmp(aux->T.Descripcion,nombre) != 0){
+		aux = aux->Siguiente;
+	}
+	if (aux != NULL){
+		aux->Siguiente = NULL;
+		return aux;		
+	}
+	else{
+		printf("su tarea no existe");
+		return NULL;
+	}
+
+}
+
+
+
+
+
+void mostrar(nodo **star){
+	nodo *t = *star;
+	
+	while(t !=NULL){
+		printf("\nID: %d",t->T.TareaID);
+    	printf("\nDescripcion: %s",t->T.Descripcion);
+    	printf("\nDuracion: %d\n",t->T.Duracion);
+    	t = t->Siguiente;
+	}
+
 }
